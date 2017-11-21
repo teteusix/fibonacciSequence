3a62da63111e416e332830f25bb22b82e90ea52f
teteusix <matheus.hf.ramos@gmail.com>
2017-11-21 23:09:04 +0100

    Insert the fibonacci sequence and adjust message text

--- app.js
+++ app.js
@@ -42,12 +42,13 @@ function checkFibonacciNumber() {
 
 
   if (fNumber == usernumber) {
-    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#27ae60;margin-right:15px;">&#10003;</span>'+usernumber+' is a Fibonacci Number</h2>'+
-    '<p>Your number <strong>'+usernumber+'</strong> is a Fibonacci Number</p>';
+    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#27ae60;margin-right:10px;">&#10003;</span>'+usernumber+' is a Fibonacci Number!</h2>'+
+    '<p>Your number <strong>'+usernumber+'</strong> is a Fibonacci Number.</p>';
   } else {
-    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#c0392b;margin-right:15px;">&#10007;</span>'+usernumber+' is not a Fibonacci Number.</h2>'+
+    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#c0392b;margin-right:10px;">&#10007;</span>'+usernumber+' is not a Fibonacci Number!</h2>'+
     '<p>Your number <strong>'+usernumber+'</strong> is not a Fibonacci Number.</p>'+
-    '<p>The nearby Fibonacci numbers are:</p><p><strong>'+fibonacciSequence[fibonacciSequence.length - 2]+' </strong>&#9667;<strong> '+usernumber+' </strong>&#9657; <strong> '+fibonacciSequence[fibonacciSequence.length - 1]+'</strong><p>';
+    '<p>The nearby Fibonacci numbers are:</p><p><i>'+fibonacciSequence[fibonacciSequence.length - 2]+' </i>&#9667;<strong> '+usernumber+' </strong>&#9657; <i> '+fibonacciSequence[fibonacciSequence.length - 1]+'</i><p>';
+    document.getElementsByClassName('fibonacciSequence')[0].innerHTML = fibonacciSequence;
   }
   restartFibonacci();
 }
