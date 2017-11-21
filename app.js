ed5c9a8f1edba808b1e53737915e750b7273d629
teteusix <matheus.hf.ramos@gmail.com>
2017-11-21 22:26:48 +0100

    create func to restart array of fibonacci sequence and check user number

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var fibonacciSequence = [0]
+var fibonacciSequence = [0];
 fibonacciSequence.push(1); // add to start a fibonacci sequence
-
 var fNumber = fibonacciSequence[fibonacciSequence.length - 1]; // define the fibonacci number
 fibonacciSequence.push(fNumber);
+var usernumber = document.getElementById('usernumber').value;
+var checkNumber = document.getElementById('checkNumber');
 
-console.log('Fibonacci number is '+fNumber);
+// RESTART ARRAY WITH A FIBONACCI SEQUENCE
+function restartFibonacci() {
+  fibonacciSequence = [0];
+  fibonacciSequence.push(1); // add to start a fibonacci sequence
 
+  fNumber = fibonacciSequence[fibonacciSequence.length - 1]; // define the fibonacci number
+  fibonacciSequence.push(fNumber);
+
+  return fNumber;
+}
+
+// GENERATE A FIBONACCI NUMBER
 function genFibonacciNumber() {
   fNumber = fibonacciSequence[fibonacciSequence.length - 1]; // redefine the value
   console.log('Now Fibonacci number is '+fNumber);
@@ -15,4 +26,16 @@ function genFibonacciNumber() {
   fibonacciSequence.push(fNumber); // insert the new number
 
   return fNumber;
+}
+
+// CHECK IF THE USER NUMBER IS FIBONACCI NUMBER
+function checkFibonacciNumber() {
+  var usernumber = document.getElementById('usernumber').value;
+  do {
+    genFibonacciNumber();
+    result = fNumber;
+  } while (fNumber < usernumber);
+
+  document.getElementsByTagName('p')[0].innerHTML = result;
+  startFibonacci();
 }
