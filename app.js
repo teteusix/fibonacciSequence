c38944f7678948d75975734bd47d44da1cc8eefb
teteusix <matheus.hf.ramos@gmail.com>
2017-11-21 22:46:40 +0100

    added the messages and correct rule

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var fibonacciSequence = [0];
 fibonacciSequence.push(1); // add to start a fibonacci sequence
 var fNumber = fibonacciSequence[fibonacciSequence.length - 1]; // define the fibonacci number
 fibonacciSequence.push(fNumber);
-var usernumber = document.getElementById('usernumber').value;
 var checkNumber = document.getElementById('checkNumber');
+
+// checkNumber.addEventListener('click', function() {
+//   checkFibonacciNumber();
+// });
+checkNumber.addEventListener('click', checkFibonacciNumber);
 
 // RESTART ARRAY WITH A FIBONACCI SEQUENCE
 function restartFibonacci() {
@@ -31,11 +35,16 @@ function genFibonacciNumber() {
 // CHECK IF THE USER NUMBER IS FIBONACCI NUMBER
 function checkFibonacciNumber() {
   var usernumber = document.getElementById('usernumber').value;
+
   do {
     genFibonacciNumber();
-    result = fNumber;
   } while (fNumber < usernumber);
 
-  document.getElementsByTagName('p')[0].innerHTML = result;
-  startFibonacci();
+
+  if (fNumber == usernumber) {
+    document.getElementsByTagName('p')[0].innerHTML = 'Your number <strong>'+usernumber+'</strong> is a Fibonacci Number';
+  } else {
+    document.getElementsByTagName('p')[0].innerHTML = 'Your number <strong>'+usernumber+'</strong> is a Fibonacci Number. The next numbers are: previous <i>'+fibonacciSequence[fibonacciSequence.length - 2]+'</i> and then <i>'+fibonacciSequence[fibonacciSequence.length - 1]+'</i>';
+  }
+  restartFibonacci();
 }
