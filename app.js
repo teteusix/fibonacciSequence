ed8c84fdbff357dc5213416d3998bcdfa6c49d07
teteusix <matheus.hf.ramos@gmail.com>
2017-11-22 17:50:05 +0100

    inserted function to validate input

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ fibonacciSequence.push(1); // add to start a fibonacci sequence
 var fNumber = fibonacciSequence[fibonacciSequence.length - 1]; // define the fibonacci number
 fibonacciSequence.push(fNumber);
 var checkNumber = document.getElementById('checkNumber');
+var usernumber = document.getElementById('usernumber');
 
 // checkNumber.addEventListener('click', function() {
 //   checkFibonacciNumber();
 // });
-checkNumber.addEventListener('click', checkFibonacciNumber);
+checkNumber.addEventListener('click', inputValid);
+// checkNumber.addEventListener('click', checkFibonacciNumber);
 
 // RESTART ARRAY WITH A FIBONACCI SEQUENCE
 function restartFibonacci() {
@@ -32,22 +34,32 @@ function genFibonacciNumber() {
   return fNumber;
 }
 
+
+// INPUT VALIDADION
+function inputValid() {
+  if (usernumber.value == "") {
+    console.log('Ta vazio');
+  }else {
+    console.log('Tem coisa');
+    // checkFibonacciNumber();
+  }
+  return true;
+}
+
 // CHECK IF THE USER NUMBER IS FIBONACCI NUMBER
 function checkFibonacciNumber() {
-  var usernumber = document.getElementById('usernumber').value;
-
   do {
     genFibonacciNumber();
-  } while (fNumber < usernumber);
+  } while (fNumber < usernumber.value);
 
 
-  if (fNumber == usernumber) {
-    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#27ae60;margin-right:10px;">&#10003;</span>'+usernumber+' is a Fibonacci Number!</h2>'+
-    '<p>Your number <strong>'+usernumber+'</strong> is a Fibonacci Number.</p>';
+  if (fNumber == usernumber.value) {
+    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#27ae60;margin-right:10px;">&#10003;</span>'+usernumber.value+' is a Fibonacci Number!</h2>'+
+    '<p>Your number <strong>'+usernumber.value+'</strong> is a Fibonacci Number.</p>';
   } else {
-    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#c0392b;margin-right:10px;">&#10007;</span>'+usernumber+' is not a Fibonacci Number!</h2>'+
-    '<p>Your number <strong>'+usernumber+'</strong> is not a Fibonacci Number.</p>'+
-    '<p>The nearby Fibonacci numbers are:</p><p><i>'+fibonacciSequence[fibonacciSequence.length - 2]+' </i>&#9667;<strong> '+usernumber+' </strong>&#9657; <i> '+fibonacciSequence[fibonacciSequence.length - 1]+'</i><p>';
+    document.getElementsByClassName('message')[0].innerHTML = '<h2><span style="color:#c0392b;margin-right:10px;">&#10007;</span>'+usernumber.value+' is not a Fibonacci Number!</h2>'+
+    '<p>Your number <strong>'+usernumber.value+'</strong> is not a Fibonacci Number.</p>'+
+    '<p>The nearby Fibonacci numbers are:</p><p><i>'+fibonacciSequence[fibonacciSequence.length - 2]+' </i>&#9667;<strong> '+usernumber.value+' </strong>&#9657; <i> '+fibonacciSequence[fibonacciSequence.length - 1]+'</i><p>';
     document.getElementsByClassName('fibonacciSequence')[0].innerHTML = fibonacciSequence;
   }
   restartFibonacci();
